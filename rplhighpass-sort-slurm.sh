2fb95c53d4b455a6a0f00ccfff46b752706504e4
AWS ParallelCluster user <ec2-user@ip-10-0-7-72.ap-southeast-1.compute.internal>
2021-10-30 16:14:12 +0000

    base env does not have any libraries

--- rplhighpass-sort-slurm.sh
+++ rplhighpass-sort-slurm.sh
@@ -18,12 +18,12 @@ source ~/.bashrc
 envarg=`python /data/src/PyHipp/envlist.py`
 conda activate $envarg
 python -u -c "import PyHipp as pyh; \
-	import time; \
-	pyh.RPLHighPass(saveLevel=1); \
-	from PyHipp import mountain_batch; \
-	mountain_batch.mountain_batch(); \
-	from PyHipp import export_mountain_cells; \
-	export_mountain_cells.export_mountain_cells(); \
-	print(time.localtime());"
-conda deactivate 
+import time; \
+pyh.RPLHighPass(saveLevel=1); \
+from PyHipp import mountain_batch; \
+mountain_batch.mountain_batch(); \
+from PyHipp import export_mountain_cells; \
+export_mountain_cells.export_mountain_cells(); \
+print(time.localtime());"
+conda activate env1 
 python /data/src/PyHipp/envlist.py $envarg
